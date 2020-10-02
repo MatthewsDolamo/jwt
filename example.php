c4fe1c2f5dd05614168b063c06e0f3a387b7a6a2
Matthews Dolamo <matthewsd@ince.co.za>
2020-10-02 17:51:04 +0200

    first commit

---
+++ example.php
@@ -0,0 +1,63 @@
+<?php
+use \Firebase\JWT\JWT;
+require_once 'library/jwt.php';
+
+/*
+ * Reading the Private Key
+ */
+$fp1 = fopen("keys/rsa_private.pem", "r");
+$privateKey = fread($fp1, 8192);
+fclose($fp1);
+
+
+/*
+    * Reading the Public Key
+ */
+$fp2 = fopen("keys/rsa_public.pem", "r");
+$publicKey = fread($fp2, 8192);
+fclose($fp2);
+
+/*
+     *Payload to sign using the private key
+     * iat is Issued at
+     * nbf is Not Before
+     * exp is Expiration Time
+     * iis is Issuer
+     * sub is Subject
+     * aud Audience
+ */
+$payload = array(
+        "LoggedInAs"=> "John Doe",
+        "ViewAs" => "John",
+        "AccountNo" =>  "1234 , 5678, 97879",
+        "iss" => "https://uat-absa.co.za:443/signer",
+        "aud" => "https://uat-absa.co.za:443/signer/ica",
+        "iat" => time() ,
+        "nbf" => time() ,
+        "exp" => (time() + 120)
+);
+
+/*
+    * Signing the payload
+ */
+$jwt = JWT::encode($payload, $privateKey, 'RS256');
+echo "Encoded<br>";
+print_r($jwt);
+
+
+/*
+    * Verifying the payload
+ */
+$decoded = JWT::decode($jwt, $publicKey, array('RS256'));
+
+
+/*
+ NOTE: This will now be an object instead of an associative array. To get
+ an associative array, you will need to cast it as such:
+*/
+echo "<br>";
+echo "<br>";
+$decoded_array = (array) $decoded;
+print_r($decoded) ;
+echo "</pre>";
+
---
+++ library/jwt.php
@@ -0,0 +1,9 @@
+<?php
+
+use \Firebase\JWT\JWT;
+
+require_once ( 'jwt/BeforeValidException.php');
+require_once ( 'jwt/ExpiredException.php');
+require_once ( 'jwt/SignatureInvalidException.php');
+require_once ( 'jwt/jwt.php');
+?>
